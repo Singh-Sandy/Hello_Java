ef1b16e82fc75ce4d5baa910a45b64309f7a14dc
Sandeep Singh <singh.sandi10@gmail.com>
2020-03-31 02:58:51 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Print "Hello World" in the terminal window
+		System.out.println("Hello World");
+	}
+
+}
